73c08f706002f94ebaa984e50974ffacb31de1fb
Alvin <fjsmhjj@163.com>
2017-11-28 14:41:59 +0800

    Initial commit

---
+++ py3.6test.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import sys
+import numpy as np
+import pandas as pd
+
+# df=pd.DataFrame(np.random.randn(10,4))
+# print(df)
+#
+# b={x :y for x in [1,2,3] for y in [4,5,6]}
+# print (b)
+
+a=['1242061', '']
+ # ['739368', ''],
+ # ['1421161', ''],
+ # ['1242061', ''],
+ # ['1467263', ''],]]
+print (a)
+
+c=[y for x in a for y in x ]
+print (c)
+#
+# a = 5
+# print (sys.path)
+
+
